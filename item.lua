6544279bef838a08d4f373049a7d7098317b7ef5
tony capone <87581644+TonyCapon3@users.noreply.github.com>
2023-01-14 12:11:07 -0500

    sv-side updates

--- item.lua
+++ item.lua
@@ -1,4 +1,16 @@
+--ADD IN SERVER SIDE IF USING AS SNIPPET
+
 QBCore.Functions.CreateUseableItem("gift",function(source, item)
-    local Player = QBCore.Functions.GetPlayer(source)
     TriggerClientEvent("consumables:client:openGiftbox", source, item.name)
+end)
+
+RegisterNetEvent('consumables:server:giftFinished', function()
+    local Player = QBCore.Functions.GetPlayer(source)
+    if Player.Functions.RemoveItem('gift', 1) then
+        TriggerClientEvent("inventory:client:ItemBox",source, QBCore.Shared.Items['gift'], "remove")
+        for name, amt in pairs(Config.GiftItems) do
+            Player.Functions.AddItem(name, amt)
+            TriggerClientEvent("inventory:client:ItemBox", source, QBCore.Shared.Items[name], "add")
+        end
+    end
 end)
