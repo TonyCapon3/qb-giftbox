69e9160770745b247b54a914d1ed417c50972c2c
tony capone <87581644+TonyCapon3@users.noreply.github.com>
2023-01-14 12:17:03 -0500

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,10 @@
+Config = {}
+
+Config.GiftItems = {
+    ['lockpick'] = 5,           --value is the amount of each item, example: lockpick has an amount of 5
+    ['sandwich'] = 5,
+    ['water_bottle'] = 5,
+    ['joint'] = 3,
+    ['bandage'] = 10,
+    ['oxy'] = 4,
+}
