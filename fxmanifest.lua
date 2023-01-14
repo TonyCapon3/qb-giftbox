94973f618d5aa1b6823becc250b347a9f0985a9e
tony capone <87581644+TonyCapon3@users.noreply.github.com>
2023-01-14 12:16:27 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+description 'Gift Box'    --Highly recommend following the MD to save that 1 extra resource space
+author 'tony.647#0778'
+version '2.0.0'
+
+client_scripts {
+    'client.lua',
+}
+
+server_scripts {
+    'server.lua'
+}
+
+shared_scripts {
+    'config.lua'
+}
